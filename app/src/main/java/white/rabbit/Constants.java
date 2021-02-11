19cbd475553651aef03dd1068580735b37f763d6
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-02-11 20:09:37 +0200

    Added word list

--- app/src/main/java/white/rabbit/Constants.java
+++ app/src/main/java/white/rabbit/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     }
 
     public static final String ANAGRAM_PHRASE = "anagram-phrase";
+    public static final String WORD_LIST = "word-list";
     public static final String ACCESS_DENIED_EXCEPTION = "Denied access for creating file";
     public static final int BUFFER_SIZE = 2 * 1024 * 1024;
 }
