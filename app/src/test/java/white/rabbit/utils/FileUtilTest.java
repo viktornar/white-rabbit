062c085ebdc4352bd391e23c9d4bf0e6fc303fa8
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-02-11 22:28:25 +0200

    Added file util test for reading file.

---
+++ app/src/test/java/white/rabbit/utils/FileUtilTest.java
@@ -0,0 +1,14 @@
+package white.rabbit.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilTest {
+
+    @Test
+    void readData() {
+        var lines = FileUtil.readData("../wordlist");
+        assertEquals(99175, lines.size());
+    }
+}
