5c9978a066f755b4d9d303ed9c39abfcca70e743
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-02-11 22:29:31 +0200

    Added additional constants for all arguments.

--- app/src/main/java/white/rabbit/Constants.java
+++ app/src/main/java/white/rabbit/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
     public static final String ANAGRAM_PHRASE = "anagram-phrase";
     public static final String WORD_LIST = "word-list";
     public static final String MD5SUM = "md5sum";
+    public static final String ANAGRAM_DETECTION_TYPE = "anagram-detection-type";
     public static final String ACCESS_DENIED_EXCEPTION = "Denied access for creating file";
     public static final int BUFFER_SIZE = 2 * 1024 * 1024;
 }
