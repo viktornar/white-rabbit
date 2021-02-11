d74fbc6d919258e99737ade1eb8f20c028254b50
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-02-11 21:37:33 +0200

    Added md5sum argument name.

--- app/src/main/java/white/rabbit/Constants.java
+++ app/src/main/java/white/rabbit/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 
     public static final String ANAGRAM_PHRASE = "anagram-phrase";
     public static final String WORD_LIST = "word-list";
+    public static final String MD5SUM = "md5sum";
     public static final String ACCESS_DENIED_EXCEPTION = "Denied access for creating file";
     public static final int BUFFER_SIZE = 2 * 1024 * 1024;
 }
