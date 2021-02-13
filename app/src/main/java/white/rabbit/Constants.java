f82ee18e1cdfe508ee3ee9810fd2b46feb57ce9b
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-02-13 13:49:20 +0200

    Code cleanup.

--- app/src/main/java/white/rabbit/Constants.java
+++ app/src/main/java/white/rabbit/Constants.java
@@ -5,9 +5,9 @@ public class Constants {
 
     }
 
+    public static final String ANAGRAM_PHRASE = "anagram-phrase";
     public static final String WORD_LIST_LOCATION = "word-list-location";
     public static final String MD5HASH = "md5hash";
-    public static final String ANAGRAM_DETECTION_TYPE = "anagram-detection-type";
     public static final String ACCESS_DENIED_EXCEPTION = "Denied access for creating file";
     public static final int BUFFER_SIZE = 2 * 1024 * 1024;
     public static final int MAX_WORD_COUNT = 4;
